5811c3d5165c3cb20f83c66268916325d24c7e7f
Pedro Aires <1212436@sga.pucminas.br>
2022-02-11 18:41:25 -0300

    testes em <Home /> com coverage = 100%

--- project-1/src/template/home/Home.spec.jsx
+++ project-1/src/template/home/Home.spec.jsx
@@ -1,5 +1,137 @@
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import userEvent from '@testing-library/user-event';
+
+import { Home } from '.';
+
+const handlers = [
+  rest.get('*jsonplaceholder.typicode.com*', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          userId: 1,
+          id: 1,
+          title: 'title1',
+          body: 'body1',
+          url: 'img1.jpg',
+        },
+        {
+          userId: 2,
+          id: 2,
+          title: 'title2',
+          body: 'body2',
+          url: 'img2.jpg',
+        },
+        {
+          userId: 3,
+          id: 3,
+          title: 'title3',
+          body: 'body3',
+          url: 'img3.jpg',
+        },
+        {
+          userId: 4,
+          id: 4,
+          title: 'title4',
+          body: 'body4',
+          url: 'img4.jpg',
+        },
+        {
+          userId: 5,
+          id: 5,
+          title: 'title5',
+          body: 'body5',
+          url: 'img5.jpg',
+        },
+        {
+          userId: 6,
+          id: 6,
+          title: 'title6',
+          body: 'body6',
+          url: 'img6.jpg',
+        },
+        {
+          userId: 7,
+          id: 7,
+          title: 'title7',
+          body: 'body7',
+          url: 'img7.jpg',
+        },
+      ]),
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
 describe('<Home />', () => {
-  it('is a dummy test', () => {
-    expect(1).not.toBe(2);
+  beforeAll(() => server.listen());
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => server.close());
+
+  it('should render searchInput, posts and "load more" button.', async () => {
+    render(<Home />);
+    expect.assertions(3);
+
+    const noMorePosts = screen.getByText('Não existem posts =(');
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(search).toBeInTheDocument();
+
+    const images = screen.getAllByRole('img', { name: /title/i });
+    expect(images).toHaveLength(6);
+
+    const button = screen.getByRole('button', { name: /load more posts/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should search for posts', async () => {
+    render(<Home />);
+    expect.assertions(10);
+
+    const noMorePosts = screen.getByText('Não existem posts =(');
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(search).toBeInTheDocument();
+
+    expect(screen.getByRole('heading', { name: 'title1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title7' })).not.toBeInTheDocument();
+
+    userEvent.type(search, 'title7');
+
+    expect(screen.queryByRole('heading', { name: 'title1' })).not.toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title2' })).not.toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title7' })).toBeInTheDocument();
+
+    userEvent.clear(search);
+    // clearing Input and backing to the begin.
+    expect(screen.getByRole('heading', { name: 'title1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title7' })).not.toBeInTheDocument();
+  });
+
+  it('should load more posts when "button" clicked .', async () => {
+    render(<Home />);
+    //expect.assertions(3);
+
+    const noMorePosts = screen.getByText('Não existem posts =(');
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const button = screen.getByRole('button', { name: /load more posts/i });
+    userEvent.click(button);
+
+    expect(screen.getByRole('heading', { name: 'title7' })).toBeInTheDocument();
+
+    expect(button).toBeDisabled();
+
+    screen.getByRole('button', { name: /end/i });
+
+    screen.debug();
   });
 });
